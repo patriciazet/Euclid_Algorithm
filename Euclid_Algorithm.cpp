27d6622a24fd43d7e778a9e028f99dd40ee7451d
Patrycja Z <patrycja.zet3@gmail.com>
2022-07-08 21:55:47 +0200

    First commit.

---
+++ Euclid_Algorithm.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Euclid_Algorithm
+{
+    
+public:
+    int calculate_NWD(int a, int b)
+    {
+        int x = 0;
+        
+        do
+        {
+            x = b;
+            b = a % b;
+            a = x;
+        } while (b != 0);
+        return a;
+    }
+};
+
+int main()
+{
+    Euclid_Algorithm ea;
+    std::cout << "NWD is: " << ea.calculate_NWD(27, 54) << std::endl;
+}
+
+
+
+
